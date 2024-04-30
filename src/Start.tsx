de527955335c2a804696ad9658e8e8b31db4613a
@Cesarscc Cesarscc <ccoloradoc@uni.pe>
2024-04-30 12:26:24 -0500

    feat: game-finish

---
+++ src/Start.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@mui/material"
+import { useQuestionsStore } from "./store/questions"
+
+const LIMIT_QUESTIONS = 10
+
+export const Start = () => {
+
+    const fetcQuestions = useQuestionsStore(state => state.fetchQuestion);
+
+    const handleClick = () => {
+        fetcQuestions(LIMIT_QUESTIONS);
+    }
+
+    return (
+        <Button onClick={handleClick} variant="contained">¡Empezar!</Button>
+    )
+
+}
---
+++ src/store/questions.ts
@@ -0,0 +1,88 @@
+import { create } from "zustand";
+import { type Question } from "../types";
+import confetti from "canvas-confetti";
+import { persist, devtools } from "zustand/middleware";
+
+interface State {
+  questions: Question[];
+  currentQuestion: number;
+  fetchQuestion: (limit: number) => Promise<void>;
+  selectAnswer: (questionId: number, answerIndex: number) => void;
+  goNextQuestion: () => void;
+  goPreviousQuestion: () => void;
+}
+
+const API_URL = import.meta.env.PROD
+  ? "http://cesar-scc-js-quizz.surge.sh/"
+  : "http://localhost:5173/";
+
+export const useQuestionsStore = create<State>()(
+  devtools(
+    persist(
+      (set, get) => {
+        return {
+          questions: [],
+          currentQuestion: 0,
+          fetchQuestion: async (limit: number) => {
+            const res = await fetch(`${API_URL}data.json`);
+            const json = await res.json();
+            const questions = json
+              .sort(() => Math.random() - 0.5)
+              .slice(0, limit);
+            set({ questions }, false, "FETCH_QUESTIONS");
+          },
+          selectAnswer: (questionId: number, answerIndex: number) => {
+            const { questions } = get();
+            //stuctureCLone
+            const newQuestions = structuredClone(questions);
+            //encontramos el indice de la pregunta
+            const questionIndex = newQuestions.findIndex(
+              (q) => q.id === questionId
+            );
+            //obtenemos info de la pregunta
+            const questionInfo = newQuestions[questionIndex];
+            //verificamos que sea respuesta correcta
+            const isCorrectUserAnswer =
+              questionInfo.correctAnswer === answerIndex;
+            if (isCorrectUserAnswer) {
+              confetti();
+            }
+            //cambiar esta informacion en la copia de la pregunta
+            newQuestions[questionIndex] = {
+              ...questionInfo,
+              isCorrectUserAnswer,
+              userSelectedAnswer: answerIndex,
+            };
+
+            //actualizar el estado:
+            set({ questions: newQuestions }, false, "SELECT_ANSWER");
+          },
+          goNextQuestion: () => {
+            const { currentQuestion, questions } = get();
+            const nexQuestion = currentQuestion + 1;
+            if (nexQuestion < questions.length) {
+              set({ currentQuestion: nexQuestion }, false, "GO_NEXT_QUESTION");
+            }
+          },
+          goPreviousQuestion: () => {
+            const { currentQuestion } = get();
+            const previousQuestion = currentQuestion - 1;
+            if (previousQuestion >= 0) {
+              set(
+                { currentQuestion: previousQuestion },
+                false,
+                "GO_PREVIOUS_QUESTION"
+              );
+            }
+          },
+          reset: () => {
+            set({ currentQuestion: 0, questions: [] }, false, "RESET_GAME");
+          },
+        };
+      },
+      {
+        name: "questions",
+      }
+    )
+  )
+);
